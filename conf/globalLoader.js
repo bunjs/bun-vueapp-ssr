e5e7246d7e57f11f237712b28c3fd89d8402e923
bunko <2leg@immaster.cn>
2019-08-07 21:01:27 +0800

    更新全局类自动加载支持

--- conf/globalLoader.js
+++ conf/globalLoader.js
@@ -1,7 +1,13 @@
+/*
+ * 设置需要全局自动引入的模块路径
+ * 当你设置文件夹路径时，默认自动加载指定目录下所有的模块
+ * @param {string} path 要加载的路径
+ * @param {array}  ignore 需要忽略的目录或文件
+ * @param {boolean} isNecessary 是否必要
+ */
 module.exports = [
     {
         path: '/common',
-        // name: '',
         // ignore: '',
         isNecessary: true
     }
